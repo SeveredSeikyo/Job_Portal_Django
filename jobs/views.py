f7c7247ea6cd7dbcf5603daaf4069acb48195a70
severedseikyo <thanmaysadguru5912@gmail.com>
2025-04-24 15:22:18 +0530

    Created Jobs list

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    # try:
+    #     jobs_list=Job.objects.all()
+    # except Exception:
+    #     print(Exception)
+    #     context={'error':'Sorry No Jobs Available Right Now'}
+    #     return render(request,'home.html',context)
+    jobs_list=Job.objects.all()
+    
+    context={'jobs_list': jobs_list}
+    return render(request,'home.html',context)
